a5929db927be84bd1a333af0097eba51507ae975
Sizan458 <www.shijan23@gmail.com>
2023-09-12 20:35:11 +0600

    finish nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
+import Shopping1 from './component/shopping1'
 
 function App() {
  
 
   return (
     <>
-     
-        
+    <Shopping1></Shopping1>
     </>
   )
 }
---
+++ src/component/shopping1.jsx
@@ -0,0 +1,31 @@
+
+
+const Shopping1 = () => {
+    return (
+        <div className='p-6'>
+            <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">Shopping</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Product</a></li>
+      <li>
+        <details>
+          <summary>
+            more
+          </summary>
+          <ul className="p-2 bg-base-100">
+            <li><a>Facebook</a></li>
+            <li><a>Whatsapp</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Shopping1;
